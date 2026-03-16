cf9cfb25809343a3f2dd3f11ba9c912b4b9388f8
akashrajeshnair <nairakash2004@gmail.com>
2026-03-16 10:09:35 +0530

    initialized spring boot project in backend

---
+++ backend/src/main/java/com/vayana/VayanaApplication.java
@@ -0,0 +1,12 @@
+package com.vayana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VayanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VayanaApplication.class, args);
+    }
+}
